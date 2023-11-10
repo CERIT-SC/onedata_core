ff3083fc0746a1904b095d71df96465a543d0453
Jozef Sabo <536644@mail.muni.cz>
2023-11-10 18:55:59 +0100

    added fs2od (v1) workflow using new API (API version 1.0.2, 1.0.1, 1.0.1 (Z,Pr,Pa) needed)

--- src/views/space.py
+++ src/views/space.py
@@ -1,12 +1,17 @@
 from __future__ import print_function
 
 import json
-from typing import TypedDict
-
+import os
+import time
+from string import Template
 import flask
+import oneprovider_client
 from flask import Blueprint, request
 import onezone_client
-from onezone_client.rest import ApiException
+import onepanel_client
+from onezone_client.rest import ApiException as ApiExceptionZone
+from onepanel_client.rest import ApiException as ApiExceptionPanel
+from oneprovider_client.rest import ApiException as ApiExceptionProvider
 from pprint import pprint
 from utils import filesystem
 
@@ -35,33 +40,156 @@ def get():
 
 @space_bp.route("/create", methods=["POST"])
 def post():
-    configuration = onezone_client.configuration.Configuration()
-    configuration.host = "https://BLINDED.muni.cz/api/v3/onezone"
-    configuration.api_key['X-Auth-Token'] = "BLINDED"
+    onezone_configuration = onezone_client.configuration.Configuration()
+    onezone_configuration.host = "https://BLINDED.muni.cz/api/v3/onezone"
+    onezone_configuration.api_key['X-Auth-Token'] = "BLINDED"
+
+    onepanel_configuration = onepanel_client.configuration.Configuration()
+    onepanel_configuration.host = "https://BLINDED.muni.cz/api/v3/onepanel"
+    onepanel_configuration.api_key['X-Auth-Token'] = "BLINDED"
+
+    oneprovider_configuration = oneprovider_client.configuration.Configuration()
+    oneprovider_configuration.host = "https://BLINDED.muni.cz/api/v3/oneprovider"
+    oneprovider_configuration.api_key['X-Auth-Token'] = "BLINDED"
 
     parameters: dict = request.json
-    if {"name"}.symmetric_difference(set(parameters.keys())):
-        return "now allowed parameters"
+    if {"name", "path", "support_size", "share_description"}.symmetric_difference(set(parameters.keys())):
+        return "now enough or not allowed parameters"
 
     # create an instance of the API class
-    api_instance = onezone_client.UserApi(onezone_client.ApiClient(configuration))
+    api_instance = onezone_client.UserApi(onezone_client.ApiClient(onezone_configuration))
     body = onezone_client.SpaceCreateRequest(
         name=parameters["name"],
         # organization_name="Name of the organization got from local variable",
     )  # SpacesBody | Space creation data. (optional)
 
+    api_response = None
+
     try:
         # Create new space
         api_response = api_instance.create_user_space(body=body)
-    except ApiException as e:
+        # api_response = api_instance.create_user_space(body=body, _preload_content=False)
+    except ApiExceptionZone as e:
         print("Exception when calling SpaceApi->create_space: %s\n" % e)
 
     body, status_code, headers = api_response
+    location = headers.get("location")
+    space_id = location.split("/")[-1]
+
+    api_instance = onepanel_client.StoragesApi(onepanel_client.ApiClient(onepanel_configuration))
+    body = onepanel_client.StorageCreateRequest(
+        {
+            parameters["name"]: {
+                "type": "posix",
+                "mountPoint": parameters["path"],
+                "importedStorage": True,
+                "readonly": True
+            }
+        }
+    )
+
+    try:
+        # Add storage
+        api_response = api_instance.add_storage(body)
+        pprint(api_response)
+    except ApiExceptionPanel as e:
+        print("Exception when calling StoragesApi->add_storage: %s\n" % e)
+
+    storage_id = None
+    for storage_name, storage_info in api_response.items():
+        storage_id = storage_info["id"]
+
+    api_instance = onezone_client.TokenApi(onezone_client.ApiClient(onezone_configuration))
+    body = onezone_client.TemporaryTokenCreateRequest(
+        type={"inviteToken": {"inviteType": "supportSpace", "spaceId": space_id}},
+        caveats=[{"type": "time", "validUntil": int(time.time()) + 120}])
+
+    try:
+        # Create temporary token for current user
+        api_response = api_instance.create_temporary_token_for_current_user(body)
+        pprint(api_response)
+    except ApiExceptionZone as e:
+        print("Exception when calling TokenApi->create_temporary_token_for_current_user: %s\n" % e)
+
+    invite_token = api_response.token
+
+    api_instance = onepanel_client.SpaceSupportApi(onepanel_client.ApiClient(onepanel_configuration))
+    body = onepanel_client.SpaceSupportRequest(
+        token=invite_token,
+        size=parameters["support_size"],
+        storage_id=storage_id,
+        storage_import=onepanel_client.StorageImport(
+            mode="auto",
+            auto_storage_import_config=onepanel_client.AutoStorageImportConfig(
+                continuous_scan=True,
+                scan_interval=60,
+                detect_modifications=True,
+                detect_deletions=True
+            )),
+    )
+
+    try:
+        # Support space
+        api_response = api_instance.support_space(body)
+        pprint(api_response)
+    except ApiExceptionPanel as e:
+        print("Exception when calling SpaceSupportApi->support_space: %s\n" % e)
+
+    support_id = api_response.id
+
+    api_instance = oneprovider_client.SpaceApi(oneprovider_client.ApiClient(oneprovider_configuration))
+
+    for try_number in range(10):
+        print("Getting space info try: %d/10" % (try_number + 1))
+        time.sleep(1)
+        try:
+            # Get basic space information
+            api_response = api_instance.get_space(space_id)
+            pprint(api_response)
+            break
+        except ApiExceptionProvider as e:
+            print("Exception when calling SpaceApi->get_space: %s\n" % e)
+
+    file_id = api_response.file_id
+
+    description = create_metadata_md(parameters["name"], "Masaryk University", file_id,
+                                     os.path.join(parameters["path"] + "/SPA.yml"))
+
+    api_instance = oneprovider_client.ShareApi(oneprovider_client.ApiClient(oneprovider_configuration))
+    body = oneprovider_client.ShareCreateRequest(
+        name=parameters["name"],
+        description=description,
+        root_file_id=file_id
+    )
+
+    try:
+        # Create share
+        api_response = api_instance.create_share(body)
+        pprint(api_response)
+    except ApiExceptionProvider as e:
+        print("Exception when calling ShareApi->create_share: %s\n" % e)
+
+    share_id = api_response.share_id
+
+    api_instance = oneprovider_client.ShareApi(oneprovider_client.ApiClient(oneprovider_configuration))
+
+    try:
+        # Get share info
+        api_response = api_instance.get_share(share_id)
+        pprint(api_response)
+    except ApiExceptionProvider as e:
+        print("Exception when calling ShareApi->get_share: %s\n" % e)
+
+    share_purl = api_response.public_url
 
     response = flask.Response(json.dumps({
         "status": status_code,
-        "body": body,
-        "location": headers.get("location")
+        "space_id": space_id,
+        "space_file_id": file_id,
+        "location": location,
+        "storage_id": storage_id,
+        "support_id": support_id,
+        "public_share_url": share_purl
     }))
     response.headers["Content-Type"] = "application/json"
     return response
