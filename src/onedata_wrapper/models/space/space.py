e21e10c0d4ececee0ab7c658206d6b2df7d60903
Jozef Sabo <536644@mail.muni.cz>
2024-03-01 19:31:08 +0100

    now changing states of Space based on root_dir presence

--- src/onedata_wrapper/models/space/space.py
+++ src/onedata_wrapper/models/space/space.py
@@ -1,5 +1,9 @@
 from typing import Optional
+
+from onedata_wrapper.models.filesystem.dir_entry import DirEntry
 from onedata_wrapper.models.space.space_state import SpaceState
+from onedata_wrapper.models.space.states.inconsistent_root import InconsistentRoot
+from onedata_wrapper.models.space.states.with_root import WithRoot
 from onedata_wrapper.models.space.states.without_root import WithoutRoot
 
 
@@ -23,10 +27,38 @@ class Space:
         if providers is not None:
             self._providers = providers
 
+    def initialize_root(self):
+        init_root_dir = DirEntry(file_id=self._root_file_id)
+        self._state = InconsistentRoot(init_root_dir)
+
+    def reinit_root(self, root_dir: DirEntry):
+        if self._root_file_id != root_dir.file_id:
+            raise ValueError("FileId of root does not match with FileId of space root")
+        if not isinstance(root_dir, DirEntry):
+            raise TypeError("Root directory is not of a type DirEntry")
+
+        self._state = WithRoot(root_dir)
+
+    def discard_root(self):
+        self._state = WithoutRoot()
+
     @property
     def space_id(self):
+        if isinstance(self._state, InconsistentRoot):
+            raise ValueError("Space in InconsistentRoot state")
+
         return self._space_id
 
     @property
-    def root_dir(self):
-        return self._root_dir
+    def name(self):
+        if isinstance(self._state, InconsistentRoot):
+            raise ValueError("Space in InconsistentRoot state")
+
+        return self._name
+
+    @property
+    def root_dir(self) -> DirEntry:
+        if isinstance(self._state, WithoutRoot):
+            raise ValueError("Cannot return root directory of a space in state WithoutRoot")
+
+        return self._state.root_dir
