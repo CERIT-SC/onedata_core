0af77a00d12cc580f28ffbcff0b432f77fa7c0ee
Jozef Sabo <536644@mail.muni.cz>
2024-03-01 19:28:22 +0100

    [fix] fixed empty SpaceState __init__()

--- src/onedata_wrapper/models/space/space_state.py
+++ src/onedata_wrapper/models/space/space_state.py
@@ -3,4 +3,5 @@ from abc import ABC
 
 class SpaceState(ABC):
     def __init__(self):
-        raise NotImplementedError("This function must be implemented for all the inherited classes")
+        pass
+        # raise NotImplementedError("This function must be implemented for all the inherited classes")
