de5d930e09314b54bd5789d22ce25d49288194b9
Jozef Sabo <536644@mail.muni.cz>
2024-03-01 19:29:57 +0100

    added InconsistentRoot state

---
+++ src/onedata_wrapper/models/space/states/inconsistent_root.py
@@ -0,0 +1,12 @@
+from onedata_wrapper.models.space.space_state import SpaceState
+from onedata_wrapper.models.filesystem.dir_entry import DirEntry
+
+
+class InconsistentRoot(SpaceState):
+    def __init__(self, root_dir: DirEntry):
+        self._root_dir: DirEntry = root_dir
+        super().__init__()
+
+    @property
+    def root_dir(self):
+        return self._root_dir
