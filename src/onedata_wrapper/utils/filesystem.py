5a29a22ffeb62d18872ba4d28d6a229f9362bdb2
Jozef Sabo <536644@mail.muni.cz>
2024-03-15 21:42:44 +0100

    added docstring to filesystem util function

--- src/onedata_wrapper/utils/filesystem.py
+++ src/onedata_wrapper/utils/filesystem.py
@@ -1,5 +1,13 @@
+from typing import Optional
 
-def read_file_contents(path: str):
+
+def read_file_contents(path: str) -> Optional[str]:
+    """
+    Reads the contents of the file given by path.
+    The read is processed textually ('r' is passed as an argument to open).
+    :param path: String representing a path to the file
+    :return: String contents of the file or None if any Exception occurred
+    """
     try:
         with open(path, "r") as f:
             return f.read()
