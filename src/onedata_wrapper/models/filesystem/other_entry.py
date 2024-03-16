ea8e099e14ca212a41eae61efb1948f92a30ed23
Jozef Sabo <536644@mail.muni.cz>
2024-03-16 01:56:27 +0100

    [fix] fixed unordered parameters

--- src/onedata_wrapper/models/filesystem/other_entry.py
+++ src/onedata_wrapper/models/filesystem/other_entry.py
@@ -3,7 +3,7 @@ from onedata_wrapper.models.filesystem.filesystem_entry import FilesystemEntry
 
 
 class OtherEntry(FilesystemEntry):
-    def __init__(self, entry_type: str, file_id: str, name=None, mode=None, size=None, hard_links=None, atime=None, mtime=None,
+    def __init__(self, entry_type: str, name: str, file_id: str, mode=None, size=None, hard_links=None, atime=None, mtime=None,
                  ctime=None, owner_id=None, parent_id=None, provider_id=None, storage_user_id=None,
                  storage_group_id=None, shares=None, index=None):
         super().__init__(name, file_id, mode, size, hard_links, atime, mtime, ctime, owner_id, parent_id, provider_id,
