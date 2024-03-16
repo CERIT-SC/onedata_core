a98a3b8b46ef7780f778ceff80f14c1c18bee58d
Jozef Sabo <536644@mail.muni.cz>
2024-03-16 02:59:31 +0100

    added docstrings to converters

--- src/onedata_wrapper/converters/abstract_converter.py
+++ src/onedata_wrapper/converters/abstract_converter.py
@@ -3,7 +3,19 @@ from abc import ABC
 
 
 class AbstractConverter(ABC):
+    """
+    Class representing Abstract Converter
+
+    Converters are used to convert onedata-lib object into object understandable by this library.
+    Each descendant of this class must implement convert() function with exactly one parameter; object to convert.
+    The return value of convert() function is an object from models package of this library
+    """
     @staticmethod
     @abc.abstractmethod
     def convert(object_to_convert):
+        """
+        Converts onedata-lib object to object from models package
+        :param object_to_convert: Onedata-lib object
+        :return: Object from models package of this library
+        """
         raise NotImplementedError("This function must be implemented for all the inherited classes")
