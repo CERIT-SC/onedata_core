318e5c20e63586b4e5b2f394be1dad0c5ece88c0
Jozef Sabo <536644@mail.muni.cz>
2024-03-21 15:33:33 +0100

    removed newline between docstring and function

--- src/onedata_wrapper/models/filesystem/new_directory_request.py
+++ src/onedata_wrapper/models/filesystem/new_directory_request.py
@@ -6,7 +6,6 @@ class NewDirectoryRequest(NewEntryRequest):
     """
     Class representing New Directory Entry Request when creating Entry in Onedata
     """
-
     def request_attrs(self) -> dict[str, Union[str, bytes]]:
         out = super().request_attrs()
         additional = {
