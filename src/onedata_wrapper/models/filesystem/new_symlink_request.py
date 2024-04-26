ac41c99ad8f3966dd5284c5f00256349a62f3771
Jozef Sabo <536644@mail.muni.cz>
2024-04-26 19:38:47 +0200

    added possibility to create symling using path formatter for absolute paths

--- src/onedata_wrapper/models/filesystem/new_symlink_request.py
+++ src/onedata_wrapper/models/filesystem/new_symlink_request.py
@@ -2,6 +2,9 @@ from typing import Optional, Union
 from onedata_wrapper.models.filesystem.dir_entry import DirEntry
 from onedata_wrapper.models.filesystem.entry_request import EntryRequest
 from onedata_wrapper.models.filesystem.new_entry_request import NewEntryRequest
+from onedata_wrapper.models.space.space import Space
+from onedata_wrapper.models.space.space_request import SpaceRequest
+from onedata_wrapper.utils.formatters import PathFormatter
 
 
 class NewSymlinkRequest(NewEntryRequest):
@@ -13,11 +16,21 @@ class NewSymlinkRequest(NewEntryRequest):
 
     The `target_file_path` attribute can contain either relative or absolute path. If the path is absolute, it must
     contain prefix in format of `<__onedata_space_id:$SPACE_ID>/` where `$SPACE_ID` is id of an actual space.
+    For this purpose, the class method `from_absolute()` should be used.
+
     Yet the interspace links do not work.
     """
     def __init__(self, parent: Union[DirEntry, EntryRequest], name: str, target_file_path: str, mode: int = 644):
         self._value = target_file_path
         super().__init__(parent, name, mode)
+
+    @classmethod
+    def from_absolute(cls, parent: Union[DirEntry, EntryRequest], name: str, space: Union[Space, SpaceRequest],
+                      absolute_path: str, mode: int = 644):
+        """This method allows using a Space or SpaceRequest object and the file absolute path
+        for creation of the symlink with an absolute path
+        """
+        return cls(parent, name, PathFormatter.symlink_absolute_path(space.space_id, absolute_path), mode)
 
     def request_attrs(self) -> dict[str, Union[str, bytes]]:
         out = super().request_attrs()
